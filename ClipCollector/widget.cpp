5f334ed381138c7137c2d3c56e91fb93e47fd84f
Jayanta Debnath <jayanta.dn@gmail.com>
2018-07-01 16:48:08 +0200

    ruler between labels

--- ClipCollector/widget.cpp
+++ ClipCollector/widget.cpp
@@ -6,10 +6,15 @@ Widget::Widget(QWidget *parent) : QWidget(parent) {
     this->setGeometry(rect.width() - 300, rect.height() - 400, 300, 400);
 
     /* creating placeholders for clips */
+    QFrame* line;
     layoutClip = new QVBoxLayout(this);
     for( uint8_t i=0; i<NUM_CLIP; i++) {
         labelClip[i] = new QLabel("label0");
         layoutClip->addWidget(labelClip[i]);
+
+        line = new QFrame;
+        line->setFrameStyle(QFrame::HLine | QFrame::Sunken);
+        layoutClip->addWidget(line);
     }
 }
 
